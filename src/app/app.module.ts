cc60ec576fd99cafa8e30734fda0c3935a9ff169
tremblaycat <tremblay.cat@outlook.com>
2020-08-18 13:30:24 -0400

    lab part 2 complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodoComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TodoComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../interfaces/todo';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-todo',
@@ -22,7 +23,51 @@ export class TodoComponent implements OnInit {
     },
   ];
 
+  searchTerm: string;
+
+  showIndex: number;
+
   constructor() {}
+
+  addTask(form: NgForm) {
+    let newItem: Todo = {
+      task: form.value.task,
+      completed: false,
+    };
+    this.todos.push(newItem);
+    form.reset();
+  }
+
+  removeTask(index: number) {
+    this.todos.splice(index, 1);
+  }
+
+  completeTask(index: number) {
+    this.todos[index].completed = true;
+  }
+
+  setSearchTerm(form: NgForm) {
+    this.searchTerm = form.value.searchTerm.trim().toLowerCase();
+  }
+
+  myFilterMethod() {
+    if (!this.searchTerm) {
+      return this.todos;
+    } else {
+      return this.todos.filter((chore) => {
+        let currentTask = chore.task.toLowerCase().trim();
+        return currentTask.includes(this.searchTerm);
+      });
+    }
+  }
+
+  setShowIndex(index: number) {
+    this.showIndex = index;
+  }
+
+  resetShowIndex() {
+    this.showIndex = undefined;
+  }
 
   ngOnInit(): void {}
 }
